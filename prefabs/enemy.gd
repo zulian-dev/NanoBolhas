39aeeb3b1ec4d6a9ec3a1d76612f67c1b3a19c45
Lucas Malfertheiner <lucasmattei2000@gmail.com>
2025-01-26 13:39:58 -0300

    feat: adicionando dois inimigos

--- prefabs/enemy.gd
+++ prefabs/enemy.gd
@@ -2,11 +2,18 @@ extends Node2D
 
 
 @export var xp = 1
-
-
+const MICRO_ORG = preload("res://assets/sprites/MicroOrg.png")
+const POEIRA = preload("res://assets/sprites/poeira.png")
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var rand = randi() % 2
+	if rand == 0:
+		sprite_2d.texture = POEIRA
+	else:
+		sprite_2d.texture = MICRO_ORG
+		
 	pass # Replace with function body.
 
 
